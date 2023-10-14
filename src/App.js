e9f404c93f4d2ffcc7f9ff1bcc285d721fb90052
maricleman <DavidNMaricle@gmail.com>
2023-10-14 11:04:45 -0500

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Morning Star Baptist Church!</h1>
+      <p>This website is currently under construction. Please be patient</p>
+    </div>
+  );
+}
+
+export default App;
